64245e93c63b41fc66287124700af6b57522f76b
Austin Vieth <ozwaldvieth@gmail.com>
2021-07-20 08:02:23 -0700

    media query for Mobile: Hero

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 const NavBar = (props) => {
+  const isMobile = useMediaQuery({ query: "(max-width: 480px)" });
+
   const renderListItems = () => {
     if (!props.items) return;
     return props.items.map((item, index) => {
@@ -15,14 +18,19 @@ const NavBar = (props) => {
   return (
     <div className="nav-bar">
       <div className="nav-bar-container">
-        <ul className="nav-bar-item-list">{renderListItems()}</ul>
-        <a
-          className="secondary-btn"
-          href="Resume_v2_FullStack_Austin_Vieth.pdf"
-          download="Austin Vieth Resume"
-        >
-          Resume
-        </a>
+        {isMobile && <span class="material-icons menu-icon">menu</span>}
+        {!isMobile && (
+          <>
+            <ul className="nav-bar-item-list">{renderListItems()}</ul>
+            <a
+              className="secondary-btn"
+              href="Resume_v2_FullStack_Austin_Vieth.pdf"
+              download="Austin Vieth Resume"
+            >
+              Resume
+            </a>
+          </>
+        )}
       </div>
     </div>
   );
